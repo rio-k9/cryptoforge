572245ac5ebd2c19262fc00aa4d7d7e5a984d500
Rio Karim <rio.karim@gmail.com>
2018-06-25 07:21:03 +0100

    final

--- submit.php
+++ submit.php
@@ -2,33 +2,37 @@
     $msg = '';
     $msgClass= '';
     if(filter_has_var(INPUT_POST, 'submit')){
-        if(empty($_POST["name"]) || empty($_POST["email"]) || empty($_POST["message"])){
-                $name = htmlspecialchars($_POST['name']);
-                $email = htmlspecialchars($_POST['email']);
-                $message = htmlspecialchars($_POST['message']);
-                $toEmail = 'rkarim@websoar.co.uk';
-                $subject = 'WebSoar Message - ' .$name;
-                $body = '
-                 <h3> You have a CryptoForge Msg </h3>
-                <h4>Contact Details</h4>
-                <ul>
-                    <li>First name:' .$name .'</li>
-                    <li>Email:' .$email .'</li>
-                    <li>Message:' .$message .'</li>
-                </ul>
-                ';
-                $headers = "MIME-Version: 1.0" ."\r\n";
-                $headers .="Content-Type:text/html;charset=UTF-8" ."\r\n";
+        $name = htmlspecialchars($_POST['name']);
+        $email = htmlspecialchars($_POST['email']);
+        $message = htmlspecialchars($_POST['message']);
+        if(!empty($name) && !empty($email) && !empty($message)){
+            $toEmail = 'rkarim@websoar.co.uk';
+            $subject = 'WebSoar Message - ' .$name;
+            $body = '
+             <h3> You have a CryptoForge Msg </h3>
+            <h4>Contact Details</h4>
+            <ul>
+                <li>First name:' .$name .'</li>
+                <li>Email:' .$email .'</li>
+                <li>Message:' .$message .'</li>
+            </ul>
+            ';
+            $headers = "MIME-Version: 1.0" ."\r\n";
+            $headers .="Content-Type:text/html;charset=UTF-8" ."\r\n";
 
-                $headers .= "From: WebSoar <noreply@websoar.co.uk>" ."\r\n";
-                if(mail($toEmail, $subject, $body, $headers)){
-                    header("Location: http://cryptoforge.riokarim.com/success.html");
-                    die();
-                }
-                else {
-                    header("Location: http://cryptoforge.riokarim.com/fail.html");
-                    die();
-                }
+            $headers .= "From: WebSoar <noreply@websoar.co.uk>" ."\r\n";
+            if(mail($toEmail, $subject, $body, $headers)){
+                header("Location: http://cryptoforge.riokarim.com/success.html");
+                die();
+            }
+            else {
+                header("Location: http://cryptoforge.riokarim.com/fail.html");
+                die();
+            }
+        }
+        else {
+            header("Location: http://cryptoforge.riokarim.com/fail.html");
+            die();
         }
     }
 ?>
